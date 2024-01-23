29e3b170c42078abf47616ef38451c6e375555ad
Milan Bauwens <milan.bauwens@craftzing.com>
2024-01-23 21:45:30 +0100

    dato plugin

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import { connect } from 'datocms-plugin-sdk';
+import { connect, } from 'datocms-plugin-sdk';
 import { render } from './utils/render';
 import ConfigScreen from './entrypoints/ConfigScreen';
 import 'datocms-react-ui/styles.css';
@@ -7,4 +7,31 @@ connect({
   renderConfigScreen(ctx) {
     return render(<ConfigScreen ctx={ctx} />);
   },
+  customMarksForStructuredTextField() {
+		return [
+			{
+				id: "sub",
+				label: "Subscript",
+				icon: "subscript",
+				keyboardShortcut: "ctrl+shift+d",
+				appliedStyle: {
+					verticalAlign: "sub",
+					fontSize: "0.75rem",
+				},
+			},
+			{
+				id: "sup",
+				label: "Superscript",
+				icon: "superscript",
+				keyboardShortcut: "ctrl+shift+u",
+				appliedStyle: {
+					verticalAlign: "top",
+					fontSize: "0.75rem",
+          position: "relative",
+          top: "-1px",
+				},
+			},
+		];
+	},
 });
+
